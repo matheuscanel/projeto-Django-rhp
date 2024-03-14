e17e0a4bb0c922e752e98729d1d193120ec3906c
gbarros26 <desiqueirabarros@hotmail.com>
2024-03-14 15:38:31 -0300

    mask js html

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -12,7 +12,36 @@ $(document).ready(function () {
 
     // Aplicar a máscara ao campo de Celular
     $('input[name="celular"]').inputmask('(99) 99999-9999', { placeholder: '(__) _____-____' });
+
+
 });
+
+
+function allStaties() {
+    const estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal',
+        'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais',
+        'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte',
+        'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
+
+    //para cada novo input com id, eu devo chamar no getElementById    
+    const selectEstados1 = document.getElementById('estado_nascimento');
+    const selectEstados2 = document.getElementById('estado');
+
+    estados.forEach(function (estado) {
+        const option1 = document.createElement('option'); // Cria um novo elemento <option>
+        const option2 = document.createElement('option');
+        option1.value = estado; // Define o valor da opção como o nome do estado
+        option1.textContent = estado; // Define o texto da opção como o nome do estado
+        option2.value = estado; // Define o valor da opção como o nome do estado
+        option2.textContent = estado; // Define o texto da opção como o nome do estado
+        selectEstados1.appendChild(option1); // Adiciona a opção ao combobox
+        selectEstados2.appendChild(option2);
+    });
+
+}
+
+window.onload = allStaties;
+
 
 function validateForm() {
     var form = document.getElementById('cadastroForm');
